3759d49dd4581a27f933f778723575795c76e4b7
faik şahin bahçekapılı <62908221+zeftil@users.noreply.github.com>
2022-03-04 00:58:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PRATİK_ÖRNEK_EXAMPLE_METODLAR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanıcıdan aldığımız sayinin karesini alalım eğer karesi 25 ten büyükse ;
+            // karesi 25 'den büyüktür
+            //değilse karesi 25'ten küçüktür yazdırsın.
+
+            Console.WriteLine("birinci sayiyi giriniz:");
+            int sayi1 = int.Parse(Console.ReadLine());
+
+            int kareDeger = kareAl(sayi1);
+            if (kareDeger > 25)
+            {
+                Console.WriteLine("karesi 25 ten büyüktür.");    
+            }
+            while (kareDeger < 25)
+            {
+                Console.WriteLine("karesi 25 ten küçüktür.");
+            }
+
+            Console.ReadLine();
+            
+        }
+        static int kareAl (int a)
+        {
+            return a * a;
+        }
+    }
+}
